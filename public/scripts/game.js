1bfd63c050b6a4617c36173472fc58f2d076b28c
Lina Kausch <lina.kausch@student.howest.be>
2025-06-08 20:04:34 +0200

    getting random, sorted question for each of buttons.

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,7 +1,12 @@
+const $question = document.querySelector(".question");
+const $markBtns = document.querySelectorAll(".game button");
 
+let questions = [];
 
 const init = () => {
     getQuestions();
+    console.log($markBtns);
+    markButtons();
 }
 
 const getQuestions = async () => {
@@ -11,12 +16,39 @@ const getQuestions = async () => {
         if (!response.ok) {
             throw new Error(`Fetch failed: ${response.status}`);
         }
-
-        const json = await response.json();
-        console.log(json);
+        questions = await response.json();
+        console.log(questions);
     } catch (error) {
         console.error('Error loading questions:', error.message);
     }
+}
+
+const markButtons = () => {
+    $markBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            const mark = button.dataset.mark;
+            console.log(mark);
+            const filtered = questions.filter(q => q.mark === mark);
+            const randomQuestion = filtered[Math.floor(Math.random() * filtered.length)];
+            console.log(randomQuestion);
+
+            $question.innerHTML = `
+            ${randomQuestion.extra ? `
+                <div>
+                 <p>${randomQuestion.extra.title}</p>
+                  <img src="${randomQuestion.extra.img}" alt="${randomQuestion.extra.title}" />
+                  <p>${randomQuestion.extra.description}</p>
+                </div>` : ''}
+                <p class="type">${randomQuestion.type}</p>
+                <p><strong>${randomQuestion.task}</strong></p>
+                <p>${randomQuestion.content}</p>
+                ${randomQuestion.example ?
+                    `<div><hr>
+                    <p>Example: ${randomQuestion.example}</p>
+                    <hr></div>` : ''}
+              `;
+        })
+    })
 }
 
 init();
