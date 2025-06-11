f9c9ca52997ce4f2f9cca1a42e6a1bc918178a6f
Rudolfs Dapkuns <rudolfs.dapkuns@student.howest.be>
2025-06-11 23:05:14 +0200

    Added Supabase and map now shows the amount of people there are at each task point in the map

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/Integration4/',
     root: 'public',
     build: {
-        outDir: 'dist',
-    },
+        outDir: '../dist', // Output goes one level up from 'public' to keep things clean
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'public/index.html'),
+                intro: resolve(__dirname, 'public/intro.html'),
+                game: resolve(__dirname, 'public/game.html'),
+            }
+        }
+    }
 });
